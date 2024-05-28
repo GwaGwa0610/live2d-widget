94866a564271d288b9c2eebaa5e195ec4233e13b
GwaGwa0610 <wd63672@gmail.com>
2024-05-29 02:58:18 +0800

    GwaLive2d

--- autoload.js
+++ autoload.js
@@ -1,8 +1,8 @@
-// live2d_path 参数建议使用绝对路径
-const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
+// live2d_path 
+const live2d_path ="https://fastly.jsdelivr.net/gh/GwaGwa0610/live2d-widget@latest/autoload.js";
 //const live2d_path = "/live2d-widget/";
+//GwaGwa0610 	
 
-// 封装异步加载资源的方法
 function loadExternalResource(url, type) {
 	return new Promise((resolve, reject) => {
 		let tag;
@@ -24,14 +24,14 @@ function loadExternalResource(url, type) {
 	});
 }
 
-// 加载 waifu.css live2d.min.js waifu-tips.js
+
 if (screen.width >= 768) {
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
-		// 配置选项的具体用法见 README.md
+		
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
